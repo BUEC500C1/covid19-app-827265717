8e58756355f9d1eb652031deaddfa859acc88a8e
song geng <827265717@qq.com>
2020-04-24 15:04:03 -0400

    Add files via upload

---
+++ Step 2/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function HelloWorldApp() {
+  return (
+    <View style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+      <Text>Hello, world!</Text>
+    </View>
+  );
+}
